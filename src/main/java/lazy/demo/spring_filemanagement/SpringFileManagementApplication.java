b2e0acf429020b2e6898e9bfdf94db48b668c5e6
phanconghung <phanconghung200591@gmail.com>
2024-08-17 10:29:04 +0700

    master

---
+++ src/main/java/lazy/demo/spring_filemanagement/SpringFileManagementApplication.java
@@ -0,0 +1,13 @@
+package lazy.demo.spring_filemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lazy/demo/spring_filemanagement/SpringFileManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lazy.demo.spring_filemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
